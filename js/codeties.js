82d70624f782873b253340a90653ec7129278ca6
Ties Noordhuis <tiesnoordhuis@gmail.com>
2016-12-21 15:15:26 +0100

    grid en galerij optie toegevoegd

--- js/codeties.js
+++ js/codeties.js
@@ -8,3 +8,17 @@ geschreven door Ties Noordhuis
 /*variable*/
 
 var n = 0;
+
+function setGalerijStyle(n) {
+  if (n == 1) {
+    document.getElementById('myCarousel').style.display = "block";
+    document.getElementById('myGrid').style.display = "none";
+  }
+  else if (n == 2) {
+    document.getElementById('myCarousel').style.display = "none";
+    document.getElementById('myGrid').style.display = "block";
+  }
+  else {
+    window.alert("fout in setGalerijStyle");
+  }
+}
