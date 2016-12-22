79f5f39a4a09f58c013b26f41bb31f1b4dae8b86
Ties Noordhuis <tiesnoordhuis@gmail.com>
2016-12-22 01:45:09 +0100

    knoppen mooier gemaakt

--- js/codeties.js
+++ js/codeties.js
@@ -12,11 +12,15 @@ var n = 0;
 function setGalerijStyle(n) {
   if (n == 1) {
     document.getElementById('myCarousel').style.display = "block";
+    document.getElementById('setGalerijStyleSlideshow').className = "btn btn-default btnFotos active";
     document.getElementById('myGrid').style.display = "none";
+    document.getElementById('setGalerijStyleGrid').className = "btn btn-default btnFotos";
   }
   else if (n == 2) {
     document.getElementById('myCarousel').style.display = "none";
+    document.getElementById('setGalerijStyleSlideshow').className = "btn btn-default btnFotos";
     document.getElementById('myGrid').style.display = "block";
+    document.getElementById('setGalerijStyleGrid').className = "btn btn-default btnFotos active";
   }
   else {
     window.alert("fout in setGalerijStyle");
