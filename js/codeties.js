387287b523860879483b206e9f71a378d2309138
Ties Noordhuis <tiesnoordhuis@gmail.com>
2017-01-12 12:52:31 +0100

    js add grid foto's niet af

--- js/codeties.js
+++ js/codeties.js
@@ -8,6 +8,8 @@ geschreven door Ties Noordhuis
 /*variable*/
 
 var n = 0;
+var fotos = ["huis1.png", "huis2.png", "huis3.png", "P8302462.JPG", "P8302463.JPG", "P8302464.JPG", "P8302465.JPG"]
+var fotosDescription = ["vooraanzicht", "vooraanzicht", "vooraanzicht", "uitzicht balkon", "uitzicht balkon", "uitzicht balkon", "balkon"]
 
 function setGalerijStyle(n) {
   if (n == 1) {
@@ -21,8 +23,16 @@ function setGalerijStyle(n) {
     document.getElementById('setGalerijStyleSlideshow').className = "btn btn-default btnFotos";
     document.getElementById('myGrid').style.display = "block";
     document.getElementById('setGalerijStyleGrid').className = "btn btn-default btnFotos active";
+    setFotosGrid()
   }
   else {
     window.alert("fout in setGalerijStyle");
   }
+}
+
+function setFotosGrid(){
+  for (var i = 0; i < fotos.length; i++) {
+    document.getElementById("myGrid").innerHTML += "test" + i + "test";
+  }
+  window.alert("setFotosGrid");
 }
